f73c8297ef129792caf8efd5c6738c3c3389e3c9
Stanislaw Podgorski <stanislaw.podgorski@eso.org>
2024-12-18 12:39:21 +0100

    optimize day 16 switching bellman-ford into dijkstra

--- src/day16.rs
+++ src/day16.rs
@@ -48,7 +48,7 @@ impl Maze {
         Self { cells, start, end }
     }
 
-    fn bellman_ford(&self) -> (i64, HashSet<Vertex>) {
+    fn dijkstra(&self) -> (i64, HashSet<Vertex>) {
         let mut costs = HashMap::new();
         let mut pred = HashMap::new();
         for &(i, j) in self.cells.keys() {
@@ -57,46 +57,39 @@ impl Maze {
                 pred.insert(Vertex { pos: (i, j), d }, HashSet::new());
             }
         }
-        costs.insert(
-            Vertex {
-                pos: self.start,
-                d: Direction::E,
-            },
-            0,
-        );
-        for _ in 0..self.cells.keys().count() {
-            let mut relaxations = 0;
-            for &(ux, uy) in self.cells.keys() {
-                for dir in [Direction::N, Direction::S, Direction::E, Direction::W] {
-                    let u = Vertex {
-                        pos: (ux, uy),
-                        d: dir,
-                    };
-                    for (v, weight) in self.edges(u) {
-                        if self
-                            .cells
-                            .get(&v.pos)
-                            .filter(|&c| c != &CellType::Wall)
-                            .is_some()
-                        {
-                            let relaxed = costs.get(&u).unwrap() + weight;
-                            let current_cost = *costs.get(&v).unwrap();
-                            if current_cost > relaxed {
-                                relaxations += 1;
-                                costs.insert(v, relaxed);
-                                let mut predecessors = HashSet::new();
-                                predecessors.insert(u);
-                                pred.insert(v, predecessors);
-                            } else if current_cost == relaxed {
-                                pred.get_mut(&v).unwrap().insert(u);
-                            }
+        let start_vertex = Vertex {
+            pos: self.start,
+            d: Direction::E,
+        };
+        costs.insert(start_vertex, 0);
+        let mut last_relaxed = HashSet::new();
+        last_relaxed.insert(start_vertex);
+        while !last_relaxed.is_empty() {
+            let mut new_last_relaxed = HashSet::new();
+            for &u in last_relaxed.iter() {
+                for (v, weight) in self.edges(u) {
+                    if self
+                        .cells
+                        .get(&v.pos)
+                        .filter(|&c| c != &CellType::Wall)
+                        .is_some()
+                    {
+                        let relaxed = costs.get(&u).unwrap() + weight;
+                        let current_cost = *costs.get(&v).unwrap();
+                        if current_cost > relaxed {
+                            costs.insert(v, relaxed);
+                            let mut predecessors = HashSet::new();
+                            predecessors.insert(u);
+                            pred.insert(v, predecessors);
+                            new_last_relaxed.insert(v);
+                        } else if current_cost == relaxed {
+                            pred.get_mut(&v).unwrap().insert(u);
+                            new_last_relaxed.insert(v);
                         }
                     }
                 }
             }
-            if relaxations == 0 {
-                break;
-            }
+            last_relaxed = new_last_relaxed;
         }
         let (cost, last_direction) = [Direction::N, Direction::S, Direction::E, Direction::W]
             .map(|d| Vertex { pos: self.end, d })
@@ -109,6 +102,8 @@ impl Maze {
     fn edges(&self, u: Vertex) -> Vec<(Vertex, i64)> {
         let d = u.d;
         let (dx, dy) = d.deltas();
+        let (c_dx, c_dy) = d.clockwise().deltas();
+        let (cc_dx, cc_dy) = d.counter_clockwise().deltas();
         let (x, y) = u.pos;
         vec![
             (
@@ -120,17 +115,24 @@ impl Maze {
             ),
             (
                 Vertex {
-                    pos: (x, y),
-                    d: d.clockwise(),
+                    pos: (x - dx, y - dy),
+                    d: d.clockwise().clockwise(),
                 },
-                1000,
+                2001,
             ),
             (
                 Vertex {
-                    pos: (x, y),
+                    pos: (x + c_dx, y + c_dy),
+                    d: d.clockwise(),
+                },
+                1001,
+            ),
+            (
+                Vertex {
+                    pos: (x + cc_dx, y + cc_dy),
                     d: d.counter_clockwise(),
                 },
-                1000,
+                1001,
             ),
         ]
     }
@@ -188,7 +190,7 @@ impl Direction {
 pub(crate) fn solve() {
     let content = fs::read_to_string("16.txt").unwrap();
     let maze = Maze::new(&content);
-    let (cost, path) = maze.bellman_ford();
+    let (cost, path) = maze.dijkstra();
     println!("{}", cost);
     println!("{}", path.iter().map(|&v| v.pos).unique().count());
 }
